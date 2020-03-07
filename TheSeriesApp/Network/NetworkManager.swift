8606a73cc3529fa5d3abe8e378f1ecd650bc1e00
Márcio Habigzang Brufatto <mhbrufatto@gmail.com>
2020-03-07 00:11:07 -0300

    Camada de Network criada

---
+++ TheSeriesApp/Network/NetworkManager.swift
@@ -0,0 +1,44 @@
+//
+//  NetworkManager.swift
+//  TheSeriesApp
+//
+//  Created by Marcio Habigzang Brufatto on 06/03/20.
+//  Copyright © 2020 Marcio Habigzang Brufatto. All rights reserved.
+//
+
+import Foundation
+
+struct Resource<T> {
+    let url: String
+    let page: Int
+    let parse: (Data) -> T?
+}
+
+
+fileprivate struct APIConfig {
+    static let apiKey = "6f4768c52238557ac6372ea46aefbb71"
+}
+
+final class NetworkManager {
+    
+    func load<T>(resource: Resource<T>, completion: @escaping(T?) -> Void) {
+        
+        let tvShowsUrl = URLComponents(string: resource.url)!
+        var request = URLRequest(url: tvShowsUrl.url!)
+        
+        request.httpMethod = "GET"
+        
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        URLSession.shared.dataTask(with: request as URLRequest) { data, response, error in
+            if let data = data {
+                DispatchQueue.main.async {
+                    completion(resource.parse(data))
+                }
+            } else {
+                completion(resource.parse((error?.localizedDescription.data(using: .utf8))!))
+            }
+        }.resume()
+    }
+}
