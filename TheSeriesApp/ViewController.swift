8250300817695a50c7c9237db92aa233bfedca3b
Márcio Habigzang Brufatto <mhbrufatto@gmail.com>
2020-03-06 22:51:03 -0300

    Initial Commit

---
+++ TheSeriesApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheSeriesApp
+//
+//  Created by Marcio Habigzang Brufatto on 06/03/20.
+//  Copyright © 2020 Marcio Habigzang Brufatto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
